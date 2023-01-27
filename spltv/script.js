48bddd45821bf44244266b0c0bc457b22655f333
AlfaNashiha26 <103365359+AlfaNashiha26@users.noreply.github.com>
2023-01-27 13:46:32 +0700

    Add files via upload

---
+++ spltv/script.js
@@ -0,0 +1,25 @@
+function chat() {
+    const message = "hai min.. saya mau tanya tentang website anda"
+    const whatsapp = "https://wa.me/+6285323147012"
+    open(whatsapp, "_blank")
+}
+
+function myInstagram() {
+    window.location.href = "https://instagram.com/alfa.nashiha_"
+}
+
+function email() {
+    window.location.href = "mailto:alfanashiha79@gmail.com"
+}
+
+function myGithub() {
+    window.location.href = "https://github.com/AlfaNashiha26"
+}
+
+function myFacebook() {
+    window.location.href = "https://www.facebook.com/"
+}
+
+function myTwitter() {
+    window.location.href = "https://twitter.com"
+}
